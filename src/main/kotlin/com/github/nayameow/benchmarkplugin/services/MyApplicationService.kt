3c4f1fbf4ef39f1713995521654bea06ff73f028
GitHub Action <action@github.com>
2022-12-07 16:26:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/nayameow/benchmarkplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nayameow/benchmarkplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nayameow.benchmarkplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nayameow.benchmarkplugin.MyBundle
 
 class MyApplicationService {
 
